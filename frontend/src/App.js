ff95dffeb08b89b61d0916dbd2d72bc4e7eee867
Daniel Bertolozi <daniel.bertolozi@sap.com>
2020-03-25 18:16:53 -0300

    Example using component + states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
+import Header from "./Header";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Increment</button>
+    </div>
   );
 }
 
