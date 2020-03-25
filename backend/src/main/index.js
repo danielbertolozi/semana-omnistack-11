0ca5630b67f05371824be2d63902e452213a7f9d
Daniel Bertolozi <daniel.bertolozi@sap.com>
2020-03-24 22:04:48 -0300

    Add support for CORS

--- backend/src/main/index.js
+++ backend/src/main/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const ongRoutes = require("./routes/ONGs");
 const incidentRoutes = require("./routes/Incidents");
 const profileRoutes = require("./routes/Profiles");
 const sessionRoutes = require("./routes/Sessions");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(ongRoutes);
