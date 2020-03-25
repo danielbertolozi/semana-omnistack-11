5a2bbdd41fd2a277b624282908feb4b31a5975a1
Daniel Bertolozi <daniel.bertolozi@sap.com>
2020-03-25 19:35:20 -0300

    Lots of things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import Logon from "./pages/Logon";
 
 import "./global.css"
 
+import Routes from "./routes";
+
 function App() {
   return (
-    <Logon />
+    <Routes /> 
   );
 }
 
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 
 import "./styles.css";
@@ -9,18 +10,20 @@ export default function Logon() {
   return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be The Hero" />
         <form>
+          <img src={logoImg} alt="Be The Hero" />
           <h1>Faça seu logon</h1>
-          <input placeholder="Sua ID" />
-          <button className="button" type="submit">Entrar</button>
-
-          <a href="/register">
-            <FiLogIn size={16} color="#E02041" /> 
-            Não tenho cadastro
-          </a>
+          <input className="" placeholder="Sua ID" />
+          <button className="button" type="submit">
+            Entrar
+          </button>
         </form>
+        <Link className="back-link" to="/register">
+          <FiLogIn size={16} color="#E02041" />
+          Não tenho cadastro
+        </Link>
       </section>
+
       <img src={heroesImg} alt="Heroes" />
     </div>
   );
