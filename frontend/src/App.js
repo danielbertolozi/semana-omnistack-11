26eb29682268ac30f867c60f8e581dc1e17181d9
Daniel Bertolozi <daniel.bertolozi@sap.com>
2020-03-25 19:12:51 -0300

    Logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
+import Logon from "./pages/Logon";
+
+import "./global.css"
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Increment</button>
-    </div>
+    <Logon />
   );
 }
 
